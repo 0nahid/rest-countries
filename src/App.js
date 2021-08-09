ad2ee51aa5f3941d469d183586b427205f9f5eca
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-09 15:40:04 +0600

    added country count

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import Country from "./components/Country/Country";
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([])
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
       .then(response => response.json())
-      .then(data => console.log(setCountries(data)))
+      .then(data => setCountries(data))
   }, []);
-
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  }
   return (
-    <div>
+    <div className="country-body">
       <h1>Country Loaded {countries.length}</h1>
+      <h1>Country Added {cart.length} </h1>
       <ul>
-        {countries.map(country => <Country country={country}></Country>)}
+        {countries.map(country => <Country
+          country={country}
+          handleAddCountry={handleAddCountry}
+          key={country.alpha2Code} ></Country>)}
       </ul>
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Country.css';
 const Country = (props) => {
-    const { name, capital, flag, region  } = props.country;
-    const currency = props.country.currencies.name;
+    const { name, capital, flag, region } = props.country;
+    const handleAddCountry = props.handleAddCountry;
     return (
         <div className="country">
             <h1>Name: {name}</h1>
             <h2>Capital: {capital}</h2>
             <h3>Region:{region} </h3>
-            <p>Currency: {currency}</p>
             <img src={flag} alt="" />
+            <button onClick={() => handleAddCountry(props.country)} >Add Currency</button>
         </div>
     );
 };
